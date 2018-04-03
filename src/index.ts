cc2b5d2a17ecbb71641fa37c6cc33ccb6be1d416
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-04-03 11:02:17 -0300

    Adicionando melhorias e corrigindo erros

--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,47 @@ import { fillTree } from "./sintatico";
 // var entrada = "( 1 )" // ok
 // var entrada = "( x ) + 1" // ok
 
-export default function analisador(entrada:string) {
-    entrada = entrada.trim();
+function analisador(entrada: string) {
+  entrada = entrada.trim();
 
-    var tabela_tokens: Array<any> = analisadorLexico(entrada)
-    if(tabela_tokens){    
-        try{
-            var tree =  fillTree(tabela_tokens);
-            var resultInOrdem = tree.raiz.visitInOrdem().trim();
-            
-            if (resultInOrdem === entrada){
-                return result("Entrada sintaticamente correta!", 1);
-            }else{
-                return result("Entrada sintaticamente incorreta!");
-            }
-            
-        }catch(erro){
-            return result("Entrada sintaticamente incorreta!");
-        }
+  var tabela_tokens: Array<any> = analisadorLexico(entrada)
+  if (tabela_tokens) {
+    let resultLexico = lexicamente(tabela_tokens)
+    if (resultLexico.status === "erro") {
+      return result(resultLexico.msg);
     }
+
+    try {
+      var tree = fillTree(tabela_tokens);
+      var resultInOrdem = tree.raiz.visitInOrdem().trim();
+
+      if (resultInOrdem === entrada) {
+        return result("Sentença léxica e sintaticamente correta!", 1);
+      } else {
+        return result("Sentença sintaticamente incorreta!");
+      }
+
+    } catch (erro) {
+      return result("Sentença sintaticamente incorreta!");
+    }
+  }
 }
 
-function result(mensagem: string, status: number = 0){
-    if(status === 0){
-        return {msg: mensagem, status:"erro"};
-    }else{
-        return {msg: mensagem, status:"ok"};
+function lexicamente(tb_tokens: any[]) {
+  for (const infoToken of tb_tokens) {
+    if (infoToken.identificador === "-") {
+      return { msg: `Token '${infoToken.token}' não existe na gramática!`, status: 'erro' }
     }
+  }
+  return { msg: `Sentença lexicamente correta!`, status: 'ok' }
 }
+
+function result(mensagem: string, status: number = 0) {
+  if (status === 0) {
+    return { msg: mensagem, status: "erro" };
+  } else {
+    return { msg: mensagem, status: "ok" };
+  }
+}
+
+export { analisador as verificaSentenca }
--- src/sintatico.ts
+++ src/sintatico.ts
@@ -6,96 +6,98 @@ import { AbreParenteses, FechaParenteses, E, Incognita, Terminal } from "./class
 import { eh_operador } from "./lexico";
 
 
-export function fillTree(entrada: any[]){
-    var tree: Tree = new Tree();    
-    
-    tree.raiz = getNodes(entrada, tree.raiz);
+export function fillTree(entrada: any[]) {
+  var tree: Tree = new Tree();
 
-    return tree;
-    
+  tree.raiz = getNodes(entrada, tree.raiz);
+
+  return tree;
+
 }
 
-function getNodes(entrada: any[], node: Node){
-    if(matchNumero(entrada)){
-        let numero = entrada[0].token
-        node.centro = new Node(new Numero())
-        let infoNumero = new Terminal(numero)
-        infoNumero = new InfoNode(infoNumero.valor, infoNumero.tipo);
-        
-        node.centro.centro = new Node(infoNumero)
-        return node;
-    }
-    else if(matchIncognita(entrada)){
-        let incognita = entrada[0].token
-        node.centro = new Node(new Incognita())
-        let infoIncognita = new Terminal(incognita)
-        infoIncognita = new InfoNode(infoIncognita.valor, infoIncognita.tipo);
-        
-        node.centro.centro = new Node(infoIncognita)
-        return node;
-    }
-    else if(matchParentes(entrada)){
-        let nova_entrada = entrada.slice(1, entrada.length-1);
-        
-        node.esquerdo = new Node(new AbreParenteses());
-        node.direito = new Node(new FechaParenteses());
-        node.centro = getNodes(nova_entrada, new Node(new E()))
-        return node
-    }
-    else if(matchOperacao(entrada) > 0){
-        let index = matchOperacao(entrada)
-                
-        let esqOperador = entrada.slice(0, index)
-        let dirOperador = entrada.slice(index+1, entrada.length)
-        let nodeOperador = new Node(new Operador())
-        nodeOperador.centro = new Node(new Terminal('+'))
+function getNodes(entrada: any[], node: Node) {
+  if (matchNumero(entrada)) {
+    let numero = entrada[0].token
+    node.centro = new Node(new Numero())
+    let infoNumero = new Terminal(numero)
+    infoNumero = new InfoNode(infoNumero.valor, infoNumero.tipo);
 
-        node.centro = nodeOperador
-        node.esquerdo = getNodes(esqOperador, new Node(new E()))
-        node.direito = getNodes(dirOperador, new Node(new E()))
-        
-        return node
-    }
-}
+    node.centro.centro = new Node(infoNumero)
+    return node;
+  }
+  else if (matchIncognita(entrada)) {
+    let incognita = entrada[0].token
+    node.centro = new Node(new Incognita())
+    let infoIncognita = new Terminal(incognita)
+    infoIncognita = new InfoNode(infoIncognita.valor, infoIncognita.tipo);
 
-function matchNumero(entrada: any[]){
-    // Regra NUMERO
-    if (entrada.length === 1){
-        if(entrada[0].identificador === TIPO_NUMERO){
-            return true;
-        }
-    }
+    node.centro.centro = new Node(infoIncognita)
+    return node;
+  }
+  else if (matchParentes(entrada)) {
+    let nova_entrada = entrada.slice(1, entrada.length - 1);
 
-    return false;
-}
-function matchIncognita(entrada: any[]){
-    // Regra INCOGNITA
-    if (entrada.length === 1){
-        if(entrada[0].identificador === TIPO_INCOGNITA){
-            return true;
-        }
-    }
-    return false;
-}
-function matchParentes(entrada: any[]){
-    // Regra '(' E ')'
-    return entrada[0].identificador === DESC_TIPO_ABRE_PARENTESES && entrada[entrada.length - 1].identificador === DESC_TIPO_FECHA_PARENTESES;
-}
-function getStrEntrada(entrada: any[], separator = ""){
-    return entrada.map(token => token.token).join(separator)
+    node.esquerdo = new Node(new AbreParenteses());
+    node.direito = new Node(new FechaParenteses());
+    node.centro = getNodes(nova_entrada, new Node(new E()))
+    return node
+  }
+  else if (matchOperacao(entrada) > 0) {
+    let index = matchOperacao(entrada)
+
+    let esqOperador = entrada.slice(0, index)
+    let dirOperador = entrada.slice(index + 1, entrada.length)
+    let nodeOperador = new Node(new Operador())
+    let operacao = entrada[index].token
+
+    nodeOperador.centro = new Node(new Terminal(operacao))
+
+    node.centro = nodeOperador
+    node.esquerdo = getNodes(esqOperador, new Node(new E()))
+    node.direito = getNodes(dirOperador, new Node(new E()))
+
+    return node
+  }
 }
 
-function matchOperacao(entrada: any[]){
-    // Regra  E OPERADOR E
-    let entradaStr = getStrEntrada(entrada);
-    let index = 0
-    for(let char of entradaStr){
-        if(eh_operador(char)){
-            // console.log(`Operador= ${char}; pos=${index}`);
-            return index;           
-        }
-        index++;
+function matchNumero(entrada: any[]) {
+  // Regra NUMERO
+  if (entrada.length === 1) {
+    if (entrada[0].identificador === TIPO_NUMERO) {
+      return true;
     }
-    console.log(entradaStr);
-    return 0;
+  }
+
+  return false;
+}
+function matchIncognita(entrada: any[]) {
+  // Regra INCOGNITA
+  if (entrada.length === 1) {
+    if (entrada[0].identificador === TIPO_INCOGNITA) {
+      return true;
+    }
+  }
+  return false;
+}
+function matchParentes(entrada: any[]) {
+  // Regra '(' E ')'
+  return entrada[0].identificador === DESC_TIPO_ABRE_PARENTESES && entrada[entrada.length - 1].identificador === DESC_TIPO_FECHA_PARENTESES;
+}
+function getStrEntrada(entrada: any[], separator = "") {
+  return entrada.map(token => token.token).join(separator)
+}
+
+function matchOperacao(entrada: any[]) {
+  // Regra  E OPERADOR E
+  let entradaStr = getStrEntrada(entrada);
+  let index = 0
+  for (let char of entradaStr) {
+    if (eh_operador(char)) {
+      // console.log(`Operador= ${char}; pos=${index}`);
+      return index;
+    }
+    index++;
+  }
+  console.log(entradaStr);
+  return 0;
 }

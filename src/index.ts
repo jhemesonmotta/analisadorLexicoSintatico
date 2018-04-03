7cd35951950a1807e5f576a416bbea66079dc64d
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-04-03 00:25:55 -0300

    Adicionando melhorias

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
 import { analisadorLexico } from "./lexico"
 import { fillTree } from "./sintatico";
 
-var entrada = "x + ( y - x - y )"
+// var entrada = "x + ( y - x - y )"
 // var entrada = "( x )" // ok
 // var entrada = "( 1 )" // ok
 // var entrada = "( x ) + 1" // ok
 
-var tabela_tokens: Array<any> = analisadorLexico(entrada)
-if(tabela_tokens){    
-    console.log(tabela_tokens);
-    
-    fillTree(tabela_tokens)
+export default function analisador(entrada:string) {
+    entrada = entrada.trim();
+
+    var tabela_tokens: Array<any> = analisadorLexico(entrada)
+    if(tabela_tokens){    
+        try{
+            var tree =  fillTree(tabela_tokens);
+            var resultInOrdem = tree.raiz.visitInOrdem().trim();
+            
+            if (resultInOrdem === entrada){
+                return result("Entrada sintaticamente correta!", 1);
+            }else{
+                return result("Entrada sintaticamente incorreta!");
+            }
+            
+        }catch(erro){
+            return result("Entrada sintaticamente incorreta!");
+        }
+    }
+}
+
+function result(mensagem: string, status: number = 0){
+    if(status === 0){
+        return {msg: mensagem, status:"erro"};
+    }else{
+        return {msg: mensagem, status:"ok"};
+    }
 }
